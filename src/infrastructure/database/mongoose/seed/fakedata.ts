10f282310c56e8e4ec00c4e99ed50b31e19aa3f8
DanielDevHN <dangrereyes@gmail.com>
2025-03-13 22:07:44 -0600

    add fake data

---
+++ src/infrastructure/database/mongoose/seed/fakedata.ts
@@ -0,0 +1,82 @@
+export const seed = [
+    {
+        "name": "Claudio Fernández",
+        "age": 40
+    },
+    {
+        "name": "Daniel Rodríguez",
+        "age": 30
+    },
+    {
+        "name": "Laura Martínez",
+        "age": 28
+    },
+    {
+        "name": "Michael Gómez",
+        "age": 35
+    },
+    {
+        "name": "Sarah López",
+        "age": 32
+    },
+    {
+        "name": "Carlos Ramírez",
+        "age": 45
+    },
+    {
+        "name": "Emily Torres",
+        "age": 27
+    },
+    {
+        "name": "Brian Castillo",
+        "age": 38
+    },
+    {
+        "name": "Jessica Pérez",
+        "age": 31
+    },
+    {
+        "name": "James Hernández",
+        "age": 36
+    },
+    {
+        "name": "Sophia Morales",
+        "age": 29
+    },
+    {
+        "name": "Oliver Gutiérrez",
+        "age": 33
+    },
+    {
+        "name": "Ethan Sánchez",
+        "age": 37
+    },
+    {
+        "name": "Emma Navarro",
+        "age": 34
+    },
+    {
+        "name": "David Molina",
+        "age": 42
+    },
+    {
+        "name": "Lucas Herrera",
+        "age": 39
+    },
+    {
+        "name": "Isabella Domínguez",
+        "age": 26
+    },
+    {
+        "name": "Noah Cabrera",
+        "age": 41
+    },
+    {
+        "name": "Ava Rivas",
+        "age": 30
+    },
+    {
+        "name": "Liam Vargas",
+        "age": 44
+    }
+]
